f7878f231447e7653c39fe9bcddb6698d42232d1
Pablo-Caballero-Maria <pabcabmar3@alum.us.es>
2024-10-21 16:30:38 +0200

    fix: set verbosity level to Daphne

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd whope_backend
 export DJANGO_SETTINGS_MODULE=whope_backend.settings
-poetry run daphne whope_backend.asgi:application
+poetry run daphne --verbosity 1 whope_backend.asgi:application
